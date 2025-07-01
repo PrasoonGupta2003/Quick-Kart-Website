901bb6b80fe6873dc202b6a0fead3c8ea30fc17b
Prasoon Gupta <praveengupta1969.pg@gmail.com>
2025-07-01 12:19:58 +0530

    Update Orders.jsx

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -148,11 +148,18 @@ function Orders() {
                     </>
                   )}
                 </div>
-                <div className="flex items-center gap-2">
+                <div className="flex flex-col sm:flex-row sm:items-center gap-1 sm:gap-2 text-sm">
+                <div className="flex items-center gap-1">
                   <FaRupeeSign />
-                  <span className="font-semibold">{currency}{order.amount}</span>
-                  <span className="text-gray-500">via {order.paymentMethod}</span>
+                  <span className="font-semibold">
+                    {currency}
+                    {order.amount < 1000 ? order.amount + 40 : order.amount}
+                  </span>
                 </div>
+                <span className="text-gray-500">
+                  (Includes {order.amount < 1000 ? `₹${delivery_fee} Delivery Fee` : 'Free Delivery'}) via {order.paymentMethod}
+                </span>
+              </div>
               </div>
 
               {/* Delivered Snapshot */}
